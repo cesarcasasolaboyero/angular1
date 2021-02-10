6ffc01d73b5fa675ead902fd8d7ec9e07a9bdfcc
Cesar <cesarcasasolaboyero@gmail.com>
2021-02-10 22:54:18 +0100

    hola

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    HOLAAAAAA
+    HOLAAAAAAAAAA
     <router-outlet></router-outlet>
   `,
   styles: []
